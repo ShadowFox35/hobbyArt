1a356484095b1d6aa831b6193f68c71e89a4586e
ShadowFox35 <anastasia97chernova@gmail.com>
2022-12-25 00:58:17 +0300

    feat: calcTotalPrice function in basket

--- hobbyart/src/pages/Basket/Basket.tsx
+++ hobbyart/src/pages/Basket/Basket.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { goodsElemType } from '../../types/types';
 import './Basket.scss';
 
@@ -12,27 +12,20 @@ const Basket: React.FC<BasketProps> = ({ basketArray, setBasketArray }) => {
   const [totalPrice, setTotalPrice] = useState<number>(0);
 
   const amount: number = basketArray.length;
-  // const result: number[] = [];
-  // let total: number;
 
-  // for (let elem of basketArray) {
-  //   for (let key in elem) {
-  //     if (key === 'price') {
-  //       result.push(elem[key]);
-  //     }
-  //   }
-  //   console.log(result);
-  // }
+  const calcTotalPrice = () => {
+    const total: number = basketArray.reduce(function (a: number, b: goodsElemType) {
+      return a + b.price;
+    }, 0);
 
-  // if (result.length) {
-  //   total = result.reduce(function (a, b) {
-  //     return a + b;
-  //   });
-  //   console.log('total', total);
+    setTotalPrice(total);
+  };
 
-  // }
+  useEffect(() => {
+    calcTotalPrice();
+  }, [basketArray]);
 
-  // setTotalPrice(total);
+  console.log(1);
 
   return (
     <div className="container">
@@ -45,8 +38,12 @@ const Basket: React.FC<BasketProps> = ({ basketArray, setBasketArray }) => {
           <div className="basket_list">
             {basketArray.map((good: goodsElemType, index: number) => (
               <div className="basket_list_item">
-                <div className="item-img">
-                  <img src={`${process.env.PUBLIC_URL}/${good.url}`} alt={`${good.name}`} />{' '}
+                <div className="item-img-wrapper">
+                  <img
+                    className="item-img"
+                    src={`${process.env.PUBLIC_URL}/${good.url}`}
+                    alt={`${good.name}`}
+                  />{' '}
                 </div>
                 <div className="item-title">{`${good.category}${good.name}`}</div>
                 <button className="item-edit">
@@ -54,7 +51,7 @@ const Basket: React.FC<BasketProps> = ({ basketArray, setBasketArray }) => {
                   <div className="item-edit_amount">1</div>
                   <div className="item-edit_btn">+</div>
                 </button>
-                <div className="item-priсe">{`${good.price} руб.`}</div>
+                <div className="item-priсe">{`${good.price} бел. руб.`}</div>
                 <div className="item-delete"></div>
               </div>
             ))}
@@ -63,8 +60,8 @@ const Basket: React.FC<BasketProps> = ({ basketArray, setBasketArray }) => {
 
         <div className="order">
           <div className="order_details">
-            <div className="title">Колличество товаров: {amount} шт.</div>
-            <div className="priсe">{totalPrice} руб.</div>
+            <div className="title">Колличество товаров: {amount} шт.,</div>
+            <div className="priсe">{totalPrice} бел. руб.</div>
           </div>
           <button className="order_btn">Оформить заказ</button>
         </div>
